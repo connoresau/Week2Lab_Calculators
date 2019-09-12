c7d06c37f275c6d7517fba3ca081afd9c16665fe
745507 <745507@SAIT221315.ACDM.DS.SAIT.CA>
2019-09-12 13:42:35 -0600

    Create Project Created inital project

---
+++ src/week2lab_calculators/Week2Lab_Calculators.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2lab_calculators;
+
+/**
+ *
+ * @author 745507 
+ */
+public class Week2Lab_Calculators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
